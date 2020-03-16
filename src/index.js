0a860a4c0539ec6432efcbdefb4cc3abe8816d4f
Lesha Drozdov <leshadrozdov@gmail.com>
2020-03-16 21:54:55 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var str = str.split('');
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  var open = '';
+  var close = '';
+  var stock = [];
+  //получаем все открывающие скобки
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open = open + bracketsConfig[i][0];
+  }
+  //получаем все закрывающие скобки
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    close = close + bracketsConfig[i][1];
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      stock.push(str[i]);
+    } else if (stock.length == 0) {
+      console.log("false");
+      return false
+    } else if (open.indexOf(stock[stock.length - 1]) !== close.indexOf(str[i])) {
+      console.log('false');
+      return false
+    }
+  }
+  console.log('true');
+  console.log('end')
 }
